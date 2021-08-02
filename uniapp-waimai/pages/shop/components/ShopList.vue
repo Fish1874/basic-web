758b2abe80e57894295e70261377a24cffe6028d
鱼天龙 <a2433981298@163.com>
2021-08-02 21:03:34 +0800

    feat: 购物车弹出层

--- uniapp-waimai/pages/shop/components/ShopList.vue
+++ uniapp-waimai/pages/shop/components/ShopList.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="shop-list">
-		<view class="sort-area">
+		<scroll-view class="sort-area" scroll-y>
 			<view
 				class="cell" 
 				v-for="(item,index) of sort"
@@ -8,9 +8,9 @@
 				:style="{backgroundColor: current == index ? '#fff': ''}"
 				@click="current = index"
 			>{{item}}</view>
-		</view>
+		</scroll-view>
 
-		<view class="food-area"><!-- 食品区域 -->
+		<scroll-view class="food-area" scroll-y><!-- 食品区域 -->
 			<view class="title">推荐</view>
 			<view
 				 class="box"
@@ -38,18 +38,59 @@
 					<image @click="onAddFood(food)" src="~@/static/icon/ic_add.png" mode=""></image>
 				</view>
 			</view>	
-		</view>
+		</scroll-view>
 	
 		<view class="food-column"><!-- 商品栏 -->
-			<view class="food-column__icon"><image src="~@/static/shop/bulka.png" mode=""></image></view>
-			<view class="food-column__text">
-				<view class="t1">HK$<text>0.00</text></view>
-				<view class="t2">差HK$ 15.00 起送</view>
+			<view class="food-column__show" @click="onShopCart()">
+				<view class="food-column__icon"><image src="~@/static/shop/bulka.png" mode=""></image></view>
+				<view class="food-column__text">
+					<view class="t1">HK$<text>0.00</text></view>
+					<view class="t2">差HK$ 15.00 起送</view>
+				</view>
 			</view>
 			<view class="food-column__pay">
 				<button type="default">去结算</button>
 			</view>
 		</view>
+	
+	
+		<uni-popup ref="popup" type="bottom"><!-- 购物车 -->
+			<view class="shop-cart-wrap">
+				<view class="shop-cart__top">
+					<view class="text">已選商品</view>
+					<view class="clear" @click="onClear()">
+						<image src="../../../static/icon/ic_delete-white.png" mode=""></image>
+						<text>清空購物車</text>
+					</view>
+				</view>
+				<scroll-view class="shop-cart__list"  scroll-y>
+					<view
+						 class="box"
+						 v-for="(cart, index) of shopCartList"
+						 :key="index"
+					>
+						<view class="box__left">
+							<image :src="cart.photo" mode="aspectFit"></image>
+						</view>
+
+						<view class="box__center">
+							<view class="box__center-title">{{cart.title}}</view>
+							<view class="box__center-bottom">
+								<view class="money">
+									<text class="p1"><text class="unit">HK$ </text>{{cart.discount}}</text>
+								</view>
+								<view class="calculator">
+									<image src="../../../static/icon/ic_subtract.png" mode=""></image>
+									<text>1</text>
+									<image src="../../../static/icon/ic_add.png" mode=""></image>
+								</view>
+							</view>
+						</view>
+					</view>	
+				</scroll-view>
+			</view>
+		</uni-popup>
+	
 	</view>
 </template>
 
@@ -90,12 +131,59 @@
 						discount: 38.8, // 优惠价
 						original: 38    // 原价
 					},
-				]
-			};
+				],
+				
+				shopCartList: [
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+					{
+						photo: require('@/static/food/pic1.png'),
+						title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+						discount: 25,
+						num: 2
+					},
+				],
+			}
 		},
 		methods: {
 			onAddFood(v) {
 				console.log(v)
+			},
+			// 显示购物车
+			onShopCart() {
+				this.$refs.popup.open('bottom')
+			},
+			// 清除购物车
+			onClear() {
+				
 			}
 		}
 	}
@@ -121,7 +209,7 @@ $textColor:#A3A9B4;
 			color: $textColor;			
 			transition: ease-in-out .2s
 		}
-	}
+	}	
 	.food-area {
 		flex: 1;
 		width: calc(375px - 80px);
@@ -206,6 +294,7 @@ $textColor:#A3A9B4;
 	
 	.food-column {
 		@extend %flex;
+		z-index: 999;
 		position: fixed;
 		left: 50%;
 		bottom: 38px;
@@ -216,30 +305,34 @@ $textColor:#A3A9B4;
 		border-radius: 24px;
 		box-shadow: 0px 4px 8px rgba(34, 35, 38, 0.16);
 		
-		&__icon {
-			position: relative;
-			margin-left: 15px;
-			width: 60px;
-			image {
-				position: absolute;
-				top: -30px;
-				width: 100%;
-				height: 74px;
-			}
-		}
-		&__text {
-			flex: 1;
-			padding: 4px 11px;
-			color: #fff;
-			opacity: .5;
-			.t1 {
-				font-size: 14px;
-				text {
-					font-size: 17px;
+		&__show {
+			@extend %flex;
+			flex:1;
+			.food-column__icon {
+				position: relative;
+				margin-left: 15px;
+				width: 60px;
+				image {
+					position: absolute;
+					top: -30px;
+					width: 100%;
+					height: 74px;
 				}
 			}
-			.t2 {
-				font-size: 12px;
+			.food-column__text {
+				flex: 1;
+				padding: 4px 11px;
+				color: #fff;
+				opacity: .5;
+				.t1 {
+					font-size: 14px;
+					text {
+						font-size: 17px;
+					}
+				}
+				.t2 {
+					font-size: 12px;
+				}
 			}
 		}
 		&__pay {
@@ -255,6 +348,96 @@ $textColor:#A3A9B4;
 			}
 		}
 		
+	}
+
+	.shop-cart-wrap {
+		width: 100%;
+		height: 50vh;
+		padding-bottom: 114px;
+		background-color: #fff;
+		.shop-cart {
+			&__top {
+				@extend %flex-jc-sb;
+				align-items: center;
+				padding: 12px 16px;
+				font-size: 14px;
+				background-color: #F9FAFC;
+				.text {
+					font-weight: 600;
+				}
+				.clear {
+					color: $textColor;
+					font-size: 14px;
+					image {
+						width: 20px;
+						height: 20px;
+					}
+					text {
+						position: relative;
+						top: -4px;
+					}
+				}
+			}
+			
+			&__list {
+				height: 97%;
+				// overflow-y: auto;
+				.box {
+					@extend %flex;
+					padding: 8px 16px;
+					margin-bottom: 8px;
+					height: 86px;
+					background-color: #fff;
+					&__left {
+						margin-right: 8px;
+						image {
+							width: 80px;
+							height: 80px;
+						}
+					}
+					&__center {
+						@extend %flex-fd-w;
+						// justify-content: space-between;
+						margin-right: 4px;
+				
+						&-title {
+							@extend %text-wrap-overflow;
+							margin-bottom: 4px;
+							font-size: 16px;
+							font-weight: 600;
+						}
+						&-bottom {
+							@extend %flex-jc-sb;
+							.money {
+								.p1 {
+									margin-right: 8px;
+									color: $prColor;
+									font-size: 16px;
+									font-weight: 600;
+									.unit {
+										font-size: 12px;
+										font-weight: 500;
+									}
+								}
+							}
+							
+							.calculator {
+								@extend %flex-ai-c;
+								text {
+									position: relative;
+									top: -1px;
+									margin: 0 12px;
+								}
+								image {
+									width: 18px;
+									height: 18px;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
 </style>
--- uniapp-waimai/unpackage/dist/dev/mp-weixin/pages/shop/components/ShopList.js
+++ uniapp-waimai/unpackage/dist/dev/mp-weixin/pages/shop/components/ShopList.js
@@ -77,6 +77,29 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "recyclableRender", function() { return recyclableRender; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "components", function() { return components; });
 var components
+try {
+  components = {
+    uniPopup: function() {
+      return __webpack_require__.e(/*! import() | uni_modules/uni-popup/components/uni-popup/uni-popup */ "uni_modules/uni-popup/components/uni-popup/uni-popup").then(__webpack_require__.bind(null, /*! @/uni_modules/uni-popup/components/uni-popup/uni-popup.vue */ 88))
+    }
+  }
+} catch (e) {
+  if (
+    e.message.indexOf("Cannot find module") !== -1 &&
+    e.message.indexOf(".vue") !== -1
+  ) {
+    console.error(e.message)
+    console.error("1. 排查组件名称拼写是否正确")
+    console.error(
+      "2. 排查组件是否符合 easycom 规范，文档：https://uniapp.dcloud.net.cn/collocation/pages?id=easycom"
+    )
+    console.error(
+      "3. 若组件不符合 easycom 规范，需手动引入，并在 components 中注册该组件"
+    )
+  } else {
+    throw e
+  }
+}
 var render = function() {
   var _vm = this
   var _h = _vm.$createElement
@@ -180,6 +203,47 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 var _default =
 
 {
@@ -216,13 +280,60 @@ var _default =
         describe: '當天進貨 保證新鮮 口感細膩',
         discount: 38.8, // 优惠价
         original: 38 // 原价
-      }] };
+      }],
+
+
+      shopCartList: [
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 },
+
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 },
+
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 },
+
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 },
+
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 },
+
+      {
+        photo: __webpack_require__(/*! @/static/food/pic1.png */ 42),
+        title: "MAKI HOUSE 招牌壽司：日本北海甜蝦壽司",
+        discount: 25,
+        num: 2 }] };
+
 
 
   },
   methods: {
     onAddFood: function onAddFood(v) {
       console.log(v);
+    },
+    // 显示购物车
+    onShopCart: function onShopCart() {
+      this.$refs.popup.open('bottom');
+    },
+    // 清除购物车
+    onClear: function onClear() {
+
     } } };exports.default = _default;
 
 /***/ }),
